72b0b44f7a7a004ebda6a5d69b488a226108ceb9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-14 12:13:23 -0300

    Saved inicio/main.py @2024-06-14 15:13

--- inicio/main.py
+++ inicio/main.py
@@ -4,4 +4,8 @@ Xb = {local=[0,0], tit="Investigue a Areia I", texto="Lixo na Areia", foi=".i.CE
 Xc = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
 Xd = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
 Xe = {local=[0,0], tit="Investigue a Areia IV", texto="Lixo na Areia"}
+[mapa1_CENA0]
+Xb = {local=[0,0], tit="Investigue a Areia I", texto="Lixo na Areia", foi=".i.CENA0zz0zz0.0.0"}
+Xc = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
+r_caix = {img="", pr=1, foi=".i.ROOTzz0.0.0" , texto = "Obteve itens!", cena=".i.CN.CENA0.5", dif=0.04, dim=[5,3]}
 
