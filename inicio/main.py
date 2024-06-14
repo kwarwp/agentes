e2c17f480d95746dc6fdd3ff587d120269bbc100
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-14 12:44:00 -0300

    Saved inicio/main.py @2024-06-14 15:43

--- inicio/main.py
+++ inicio/main.py
@@ -1,6 +1,6 @@
 # agentes.inicio.main.py
 [mapa1_CENA0]
-Xf = {local=[0,0], img=".i.CN._PISTAS0.5", tit="Isto parece uma Peteca", texto="Eu acho que alguém jogou peteca aqui", foi=".i.ROOTzz0zz0.0.0"}
+Xf = {local=[0,0], img=".i.CN._PISTAS0.4", tit="Isto parece uma Peteca", texto="Eu acho que alguém jogou peteca aqui", foi=".i.ROOTzz0zz0.0.0"}
 Xg = {local=[0,0], img=".i.CN._PISTAS0.7", tit="Um lindo Gatinho!", texto="Acho que o gato estava brincando com a peteca."}
 #r_caix = {img="", pr=1, foi=".i.ROOTzz0.0.0" , texto = "Obteve itens!", cena=".i.CN.CENA0.5", dif=0.04, dim=[5,3]}
 
