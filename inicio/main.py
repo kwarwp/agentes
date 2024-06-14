cc8061bcfaf0a1855d9d78c3d7fc1181e7e845c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-14 12:53:31 -0300

    Saved inicio/main.py @2024-06-14 15:53

--- inicio/main.py
+++ inicio/main.py
@@ -5,7 +5,7 @@ Xg = {local=[0,0], img=".i.CN._PISTAS0.7", tit="Um lindo Gatinho!", texto="Acho 
 #r_caix = {img="", pr=1, foi=".i.ROOTzz0.0.0" , texto = "Obteve itens!", cena=".i.CN.CENA0.5", dif=0.04, dim=[5,3]}
 
 [mapa0_ROOT]
-Xb = {local=[0,0], tit="Investigue a Areia I", texto="Lixo na Areia", foi=".i.CENA0zz0zz0.0.0"}
+Xb = {local=[0,0], tit="Investigue a Praia I", texto="Procure pistas na Praia", foi=".i.CENA0zz0zz0.0.0"}
 Xc = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
 Xd = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
 Xe = {local=[0,0], tit="Investigue a Areia IV", texto="Lixo na Areia"}
