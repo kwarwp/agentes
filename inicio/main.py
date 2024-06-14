f4521d82cbac1e3260f7e6aa54ee74fb66c50821
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-14 11:08:47 -0300

    Saved inicio/main.py @2024-06-14 14:08

--- inicio/main.py
+++ inicio/main.py
@@ -1,6 +1,6 @@
 # agentes.inicio.main.py
 [mapa0_ROOT]
-Xb = {local=[0,0], tit="Investigue a Areia I", texto="Lixo na Areia"}
+Xb = {local=[0,0], tit="Investigue a Areia I", texto="Lixo na Areia", foi=".i.CENA0zz0zz0.0.0"}
 Xc = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
 Xd = {local=[0,0], tit="Investigue a Areia II", texto="Lixo na Areia"}
 Xe = {local=[0,0], tit="Investigue a Areia IV", texto="Lixo na Areia"}
